135c68cc80bb326877adc45ea739fc5ef8140d0e
ChakGonzalez <chakgonzalez85@gmail.com>
2021-11-24 21:24:23 -0600

    Actualizando archivo

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
 'use strict'
 
-console.log('Hola Mundo')
+function multiply(a,b) {
+    let r=0;
+
+    for (let index = 0; index < b; index++) {       
+        r = r+a;
+    }
+    return r;
+}
+
+document.write("Resultado: ", multiply(2,6));
